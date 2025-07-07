d89bb7d48f32bdec305f2abf2b33b6785ec371a0
Victor Shamakov <sva1.0@mail.ru>
2025-07-07 14:17:34 +0700

    Анализ данных без выделения отдельных кластеров испытуемых: кликеры, сверхточные, парадоксальные, сверхдолгие

--- render.py
+++ render.py
@@ -24,146 +24,147 @@ def render_and_save_pages(template, data_dictionary, dataset, file_name, cluster
 
 def main():
     data_dictionary = pd.read_excel("./data/data_dictionary.xlsx").to_dict(orient='records')
-    template = get_template('./templates/desc_stat_template.Rmd')
+    template = get_template('./templates/3.1_ds_template.Rmd')
     
-    dataset = open("./templates/dataset.Rmd", "r", encoding="utf8").read()
+    dataset = open("./templates/3_dataset.Rmd", "r", encoding="utf8").read()
 
     rendered_page = template.render(data_dictionary=data_dictionary, stage="acc", dataset=dataset)
 
     # accuracy
-    with open("./3.1_desc_stat_accuracy.Rmd", 'w', encoding="utf8") as file:
+    with open("./3.1_ds_acc.Rmd", 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
     # speed
     rendered_page = template.render(data_dictionary=data_dictionary, stage="spd", dataset=dataset)
 
-    with open("./3.2_desc_stat_speed.Rmd", 'w', encoding="utf8") as file:
+    with open("./3.1_ds_spd.Rmd", 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
-    template_group = get_template('./templates/desc_stat_by_group_template.Rmd')
+    template_group = get_template('./templates/3.2_ds_by_group_template.Rmd')
     rendered_page = template_group.render(data_dictionary=data_dictionary, group="stage", dataset=dataset)
 
-    with open("./3.3_desc_stat_by_stage.Rmd", 'w', encoding="utf8") as file:
+    with open("./3.2_ds_by_stage.Rmd", 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
 
-    template_gender = get_template('./templates/desc_stat_template_by_gender.Rmd')
+    template_gender = get_template('./templates/3.3_ds_template_by_gender.Rmd')
     rendered_page = template_gender.render(data_dictionary=data_dictionary, stage="acc", group="gender", dataset=dataset)
 
-    with open("./3.4_desc_stat_acc_by_gender.Rmd", 'w', encoding="utf8") as file:
+    with open("./3.3_ds_acc_by_gender.Rmd", 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
-    template_gender = get_template('./templates/desc_stat_template_by_gender.Rmd')
+    template_gender = get_template('./templates/3.3_ds_template_by_gender.Rmd')
     rendered_page = template_gender.render(data_dictionary=data_dictionary, stage="spd", group="gender", dataset=dataset)
 
-    with open("./3.5_desc_stat_spd_by_gender.Rmd", 'w', encoding="utf8") as file:
+    with open("./3.3_ds_spd_by_gender.Rmd", 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
-    template_two_groups = get_template('./templates/desc_stat_template_by_two_groups.Rmd')
+    template_two_groups = get_template('./templates/3.4_ds_template_by_two_groups.Rmd')
     rendered_page = template_two_groups.render(data_dictionary=data_dictionary, group="gender", group2="stage", dataset=dataset)
 
-    with open("./3.6_desc_stat_by_stage_and_gender.Rmd", 'w', encoding="utf8") as file:
+    with open("./3.4_ds_by_stage_and_gender.Rmd", 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
-    ## DS hclusters
-    template_group2 = get_template('./templates/desc_stat_by_group_template2.Rmd')
-    dataset_ca = open("./templates/dataset_ca.Rmd", "r", encoding="utf8").read()
+    ## DS hclusters acc
+    template_group2 = get_template('./templates/4_ds_by_group_template2.Rmd')
+    dataset_ca_acc = open("./templates/4_dataset_ca_acc.Rmd", "r", encoding="utf8").read()
 
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca, "desc_stat_by", "all_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca, "desc_stat_by", "notcorr_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca, "desc_stat_by", "tdm_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca, "desc_stat_by", "pauses_hclust", 4)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc, "ds_acc_by", "all_hclust", 4.1)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc, "ds_acc_by", "notcorr_hclust", 4.1)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc, "ds_acc_by", "tdm_hclust", 4.1)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc, "ds_acc_by", "pauses_hclust", 4.1)
 
-    ## Female
-    dataset_ca_female = open("./templates/dataset_ca_female.Rmd", "r", encoding="utf8").read()
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_female, "female_desc_stat_by", "all_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_female, "female_desc_stat_by", "notcorr_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_female, "female_desc_stat_by", "tdm_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_female, "female_desc_stat_by", "pauses_hclust", 4)
+    ## Female acc
+    dataset_ca_acc_female = open("./templates/4_dataset_ca_acc_female.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_female, "ds_acc_female_by", "all_hclust", 4.2)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_female, "ds_acc_female_by", "notcorr_hclust", 4.2)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_female, "ds_acc_female_by", "tdm_hclust", 4.2)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_female, "ds_acc_female_by", "pauses_hclust", 4.2)
 
-    ## Male
-    dataset_ca_male = open("./templates/dataset_ca_male.Rmd", "r", encoding="utf8").read()
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_male, "male_desc_stat_by", "all_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_male, "male_desc_stat_by", "notcorr_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_male, "male_desc_stat_by", "tdm_hclust", 4)
-    render_and_save_pages(template_group2, data_dictionary, dataset_ca_male, "male_desc_stat_by", "pauses_hclust", 4)
+    ## Male acc
+    dataset_ca_acc_male = open("./templates/4_dataset_ca_acc_male.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_male, "ds_acc_male_by", "all_hclust", 4.3)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_male, "ds_acc_male_by", "notcorr_hclust", 4.3)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_male, "ds_acc_male_by", "tdm_hclust", 4.3)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_acc_male, "ds_acc_male_by", "pauses_hclust", 4.3)
 
+
+    # DS hclusters spd
+    dataset_ca_spd = open("./templates/4_dataset_ca_spd.Rmd", "r", encoding="utf8").read()
+
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd, "ds_spd_by", "all_hclust", 4.4)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd, "ds_spd_by", "notcorr_hclust", 4.4)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd, "ds_spd_by", "tdm_hclust", 4.4)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd, "ds_spd_by", "pauses_hclust", 4.4)
+
+    ## Female spd
+    dataset_ca_spd_female = open("./templates/4_dataset_ca_spd_female.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_female, "ds_spd_female_by", "all_hclust", 4.5)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_female, "ds_spd_female_by", "notcorr_hclust", 4.5)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_female, "ds_spd_female_by", "tdm_hclust", 4.5)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_female, "ds_spd_female_by", "pauses_hclust", 4.5)
+
+    ## Male spd
+    dataset_ca_spd_male = open("./templates/4_dataset_ca_spd_male.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_male, "ds_spd_male_by", "all_hclust", 4.6)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_male, "ds_spd_male_by", "notcorr_hclust", 4.6)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_male, "ds_spd_male_by", "tdm_hclust", 4.6)
+    render_and_save_pages(template_group2, data_dictionary, dataset_ca_spd_male, "ds_spd_male_by", "pauses_hclust", 4.6)
 
 
     ## Psy.test and AST clusters
+    ### acc
     sp_data_dictionary = pd.read_excel("./data/sp_data_dictionary.xlsx").to_dict(orient='records')
-    ast_template = get_template('./templates/ast_template.Rmd')
-    dataset_sp = open("./templates/dataset_sp.Rmd", "r", encoding="utf8").read()
+    sp_template = get_template('./templates/5_sp_template.Rmd')
+    dataset_sp_acc = open("./templates/5_dataset_sp_acc.Rmd", "r", encoding="utf8").read()
 
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp, group="all_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_sp_desc_stat_by_all_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc, "sp_ds_acc_by", "all_hclust", 5.1)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc, "sp_ds_acc_by", "notcorr_hclust", 5.1)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc, "sp_ds_acc_by", "tdm_hclust", 5.1)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc, "sp_ds_acc_by", "pauses_hclust", 5.1)
 
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp, group="notcorr_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_sp_desc_stat_by_notcorr_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    ### Female acc
+    dataset_sp_acc_female = open("./templates/5_dataset_sp_acc_female.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_female, "sp_ds_acc_female_by", "all_hclust", 5.2)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_female, "sp_ds_acc_female_by", "notcorr_hclust", 5.2)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_female, "sp_ds_acc_female_by", "tdm_hclust", 5.2)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_female, "sp_ds_acc_female_by", "pauses_hclust", 5.2)
 
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp, group="tdm_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_sp_desc_stat_by_tdm_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    ### Male acc
+    dataset_sp_acc_male = open("./templates/5_dataset_sp_acc_male.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_male, "sp_ds_acc_male_by", "all_hclust", 5.3)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_male, "sp_ds_acc_male_by", "notcorr_hclust", 5.3)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_male, "sp_ds_acc_male_by", "tdm_hclust", 5.3)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_acc_male, "sp_ds_acc_male_by", "pauses_hclust", 5.3)
 
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp, group="pauses_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_sp_desc_stat_by_pauses_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    ### spd
+    sp_data_dictionary = pd.read_excel("./data/sp_data_dictionary.xlsx").to_dict(orient='records')
+    sp_template = get_template('./templates/5_sp_template.Rmd')
+    dataset_sp_spd = open("./templates/5_dataset_sp_spd.Rmd", "r", encoding="utf8").read()
 
-    #Female
-    dataset_sp_female = open("./templates/dataset_sp_female.Rmd", "r", encoding="utf8").read()
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_female, group="all_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_female_sp_desc_stat_by_all_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd, "sp_ds_spd_by", "all_hclust", 5.4)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd, "sp_ds_spd_by", "notcorr_hclust", 5.4)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd, "sp_ds_spd_by", "tdm_hclust", 5.4)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd, "sp_ds_spd_by", "pauses_hclust", 5.4)
 
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_female, group="notcorr_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_female_sp_desc_stat_by_notcorr_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    ### Female spd
+    dataset_sp_spd_female = open("./templates/5_dataset_sp_spd_female.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_female, "sp_ds_spd_female_by", "all_hclust", 5.5)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_female, "sp_ds_spd_female_by", "notcorr_hclust", 5.5)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_female, "sp_ds_spd_female_by", "tdm_hclust", 5.5)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_female, "sp_ds_spd_female_by", "pauses_hclust", 5.5)
 
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_female, group="tdm_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_female_sp_desc_stat_by_tdm_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_female, group="pauses_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_female_sp_desc_stat_by_pauses_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
-    #Male
-    dataset_sp_male = open("./templates/dataset_sp_male.Rmd", "r", encoding="utf8").read()
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_male, group="all_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_male_sp_desc_stat_by_all_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_male, group="notcorr_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_male_sp_desc_stat_by_notcorr_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_male, group="tdm_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_male_sp_desc_stat_by_tdm_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
-    for n in range(2, 10):
-        rendered_page = ast_template.render(data_dictionary=sp_data_dictionary, dataset=dataset_sp_male, group="pauses_hclust" + str(n))
-        with open("./5." + str((n - 1)) + "_male_sp_desc_stat_by_pauses_hclust" + str(n) + ".Rmd", 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
+    ### Male spd
+    dataset_sp_spd_male = open("./templates/5_dataset_sp_spd_male.Rmd", "r", encoding="utf8").read()
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_male, "sp_ds_spd_male_by", "all_hclust", 5.6)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_male, "sp_ds_spd_male_by", "notcorr_hclust", 5.6)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_male, "sp_ds_spd_male_by", "tdm_hclust", 5.6)
+    render_and_save_pages(sp_template, sp_data_dictionary, dataset_sp_spd_male, "sp_ds_spd_male_by", "pauses_hclust", 5.6)
 
 if __name__ == '__main__':
     main()
