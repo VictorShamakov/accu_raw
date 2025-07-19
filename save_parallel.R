65a2e3e500e0e7f5217d9c893f66c75754338641
Victor Shamakov <sva1.0@mail.ru>
2025-07-19 18:19:38 +0700

    Изменены файлы для рендеринга отчетов. Добавлен файл с параллельными вычислениями

--- save_parallel.R
+++ save_parallel.R
@@ -6,56 +6,10 @@ render_file <- function(file) {
 }
 "%+%" <- function(...){paste0(...)}
 
-## Персональный отчет
-test_id <- 85
-user_id <- 3451
-
-dir.create(file.path("data", "users", user_id))
-dir.create(file.path("data", "users", user_id, test_id))
-rmarkdown::render('./1_preprocessing_files.Rmd', output_file = file.path("data", "users", user_id, test_id, "userid_" %+% user_id %+% "_testid_" %+% test_id %+% "_personal_report.html"), params = list(user_id = user_id, test_id = test_id, data_from_db = T))
-
-
-## Персональные отчеты
-user_ids <- c(3437, 3438, 3439, 3486, 3440, 3441, 3442, 3443, 3444, 3445)
-
-for (user_id in user_ids) {
-  print(user_id)
-  rm(list=ls()[-grep("user_id|test_id|user_ids|%", ls())])
-    # dir.create(file.path("data", "users", user_id))
-    # dir.create(file.path("data", "users", user_id, test_id))
-    rmarkdown::render('./1_preprocessing_files.Rmd', output_file = file.path("data", "users", user_id, test_id, "userid_" %+% user_id %+% "_testid_" %+% test_id %+% "_personal_report.html"), params = list(user_id = user_id, test_id = test_id, data_from_db = T))
-}
-
-
-## Персональные отчеты (файлы)
-ast_results <- read_xlsx("../Accu/results/ast_impersonal_results.xlsx") # Валидные данные  теста
-ast_results <- ast_results[order(as.numeric(ast_results$user_id)),]
-ast_results$test_id <- test_id
-
-accu_data <- readRDS("./data/raw/2024-10-21 - accu.rds") # Результаты эксперимента компромисс скорость-точность
-accu_data <- merge(accu_data, select(ast_results, user_id, test_id), by = c("user_id", "test_id"), all.y = F)
-
-users <- readRDS("./data/raw/2024-10-21 - users.rds")
-users <- merge(users, select(accu_data, user_id), by = "user_id")
-
-sessions <- readRDS("./data/raw/2024-10-21 - sessions.rds") # Сессии mouse tracker'a
-sessions <- merge(sessions, select(users, user_id), by = "user_id")
-
-event_tracks <- readRDS("./data/raw/2024-10-21 - event_tracks.rds") # Треки событий
-event_tracks <- merge(event_tracks, select(sessions, session_id), by = "session_id")
-
-mouse_tracks <- readRDS("./data/raw/2024-10-21 - mouse_tracks.rds") # Треки мыши
-mouse_tracks <- merge(mouse_tracks, select(sessions, session_id), by = "session_id")
-
-for (user_id in ast_results$user_id[1:length(ast_results$user_id)]) {
-  print(user_id)
-  dir.create(paste0("./data/users/", user_id))
-  rmarkdown::render('./1_preprocessing_files.Rmd', output_file = file.path("data", "users", user_id, test_id, "userid_" %+% user_id %+% "_testid_" %+% test_id %+% "_personal_report.html"), params = list(user_id = user_id, test_id = test_id, data_from_db = F))
-}
 
 ## Описательная статистика
 ### Параллельно
-files <- list(
+files_ds <- list(
   list(input = './3.1_ds_acc.Rmd', output = './reports/3.1_ds_acc.html'),
   list(input = './3.1_ds_spd.Rmd', output = './reports/3.1_ds_spd.html'),
   list(input = './3.2_ds_by_stage.Rmd', output = './reports/3.2_ds_by_stage.html'),
@@ -63,19 +17,10 @@ files <- list(
   list(input = './3.3_ds_spd_by_gender.Rmd', output = './reports/3.3_ds_spd_by_gender.html'),
   list(input = './3.4_ds_by_stage_and_gender.Rmd', output = './reports/3.4_ds_by_stage_and_gender.html')
 )
-mclapply(files, render_file, mc.cores = detectCores())
-
-### Последовательно
-rmarkdown::render('./3.1_ds_acc.Rmd', output_file = './reports/3.1_ds_acc.html')
-rmarkdown::render('./3.1_ds_spd.Rmd', output_file = './reports/3.1_ds_spd.html')
-rmarkdown::render('./3.2_ds_by_stage.Rmd', output_file = './reports/3.2_ds_by_stage.html')
-rmarkdown::render('./3.3_ds_acc_by_gender.Rmd', output_file = './reports/3.3_ds_acc_by_gender.html')
-rmarkdown::render('./3.3_ds_spd_by_gender.Rmd', output_file = './reports/3.3_ds_spd_by_gender.html')
-rmarkdown::render('./3.4_ds_by_stage_and_gender.Rmd', output_file = './reports/3.4_ds_by_stage_and_gender.html')
 
 
 ## Кластерный анализ
-files <- list(
+files_ca <- list(
   list(input = './4.0.1_ca_acc.Rmd', output = './reports/4.0.1_ca_acc.html'),
   list(input = './4.0.2_female_ca_acc.Rmd', output = './reports/4.0.2_female_ca_acc.html'),
   list(input = './4.0.3_male_ca_acc.Rmd', output = './reports/4.0.3_male_ca_acc.html'),
@@ -84,20 +29,9 @@ files <- list(
   list(input = './4.0.6_male_ca_spd.Rmd', output = './reports/4.0.6_male_ca_spd.html')
 )
 
-system.time({
-  mclapply(files, render_file, mc.cores = detectCores())
-})
 
 
-### Последовательно
-rmarkdown::render('./4.0.1_ca_acc.Rmd', output_file = './reports/4.0.1_ca_acc.html')
-rmarkdown::render('./4.0.2_female_ca_acc.Rmd', output_file = './reports/4.0.2_female_ca_acc.html')
-rmarkdown::render('./4.0.3_male_ca_acc.Rmd', output_file = './reports/4.0.3_male_ca_acc.html')
-rmarkdown::render('./4.0.4_ca_spd.Rmd', output_file = './reports/4.0.4_ca_spd.html')
-rmarkdown::render('./4.0.5_female_ca_spd.Rmd', output_file = './reports/4.0.5_female_ca_spd.html')
-rmarkdown::render('./4.0.6_male_ca_spd.Rmd', output_file = './reports/4.0.6_male_ca_spd.html')
-
-files <- list(
+files_ds_hclust <- list(
   ## Описательная статистика кластеров
   ## Все респонденты. Задание на точность.
   list(input = './4.1.1_ds_acc_by_all_hclust2.Rmd', output = './reports/4.1.1_ds_acc_by_all_hclust2.html'),
@@ -223,8 +157,10 @@ files <- list(
   list(input = './4.6.1_ds_spd_male_by_pauses_hclust2.Rmd', output = './reports/4.6.1_ds_spd_male_by_pauses_hclust2.html'),
   list(input = './4.6.2_ds_spd_male_by_pauses_hclust3.Rmd', output = './reports/4.6.2_ds_spd_male_by_pauses_hclust3.html'),
   list(input = './4.6.3_ds_spd_male_by_pauses_hclust4.Rmd', output = './reports/4.6.3_ds_spd_male_by_pauses_hclust4.html'),
-  list(input = './4.6.4_ds_spd_male_by_pauses_hclust5.Rmd', output = './reports/4.6.4_ds_spd_male_by_pauses_hclust5.html'),
-  
+  list(input = './4.6.4_ds_spd_male_by_pauses_hclust5.Rmd', output = './reports/4.6.4_ds_spd_male_by_pauses_hclust5.html')
+)
+
+files_sp_hclust <- list(
   # Психодиагностика и кластеры
   ## Все респонденты. Задание на точность
   list(input = './5.1.1_sp_ds_acc_by_all_hclust2.Rmd', output = './reports/5.1.1_sp_ds_acc_by_all_hclust2.html'),
@@ -351,8 +287,10 @@ files <- list(
   list(input = './5.6.1_sp_ds_spd_male_by_pauses_hclust2.Rmd', output = './reports/5.6.1_sp_ds_spd_male_by_pauses_hclust2.html'),
   list(input = './5.6.2_sp_ds_spd_male_by_pauses_hclust3.Rmd', output = './reports/5.6.2_sp_ds_spd_male_by_pauses_hclust3.html'),
   list(input = './5.6.3_sp_ds_spd_male_by_pauses_hclust4.Rmd', output = './reports/5.6.3_sp_ds_spd_male_by_pauses_hclust4.html'),
-  list(input = './5.6.4_sp_ds_spd_male_by_pauses_hclust5.Rmd', output = './reports/5.6.4_sp_ds_spd_male_by_pauses_hclust5.html'),
-
+  list(input = './5.6.4_sp_ds_spd_male_by_pauses_hclust5.Rmd', output = './reports/5.6.4_sp_ds_spd_male_by_pauses_hclust5.html')
+)
+ 
+files_ds_kmclust <- list( 
   # kmclust
   ## Все респонденты. Задание на точность.
   list(input = './4.7.1_ds_acc_by_all_kmclust2.Rmd', output = './reports/4.7.1_ds_acc_by_all_kmclust2.html'),
@@ -459,8 +397,10 @@ files <- list(
   list(input = './4.12.1_ds_spd_male_by_pauses_kmclust2.Rmd', output = './reports/4.12.1_ds_spd_male_by_pauses_kmclust2.html'),
   list(input = './4.12.2_ds_spd_male_by_pauses_kmclust3.Rmd', output = './reports/4.12.2_ds_spd_male_by_pauses_kmclust3.html'),
   list(input = './4.12.3_ds_spd_male_by_pauses_kmclust4.Rmd', output = './reports/4.12.3_ds_spd_male_by_pauses_kmclust4.html'),
-  list(input = './4.12.4_ds_spd_male_by_pauses_kmclust5.Rmd', output = './reports/4.12.4_ds_spd_male_by_pauses_kmclust5.html'),
-  
+  list(input = './4.12.4_ds_spd_male_by_pauses_kmclust5.Rmd', output = './reports/4.12.4_ds_spd_male_by_pauses_kmclust5.html')
+)
+
+files_sp_kmclust <- list(
   # Психодиагностика и кластеры
   ## Все респонденты. Задание на точность
   list(input = './5.7.1_sp_ds_acc_by_all_kmclust2.Rmd', output = './reports/5.7.1_sp_ds_acc_by_all_kmclust2.html'),
@@ -569,8 +509,10 @@ files <- list(
   list(input = './5.12.1_sp_ds_spd_male_by_pauses_kmclust2.Rmd', output = './reports/5.12.1_sp_ds_spd_male_by_pauses_kmclust2.html'),
   list(input = './5.12.2_sp_ds_spd_male_by_pauses_kmclust3.Rmd', output = './reports/5.12.2_sp_ds_spd_male_by_pauses_kmclust3.html'),
   list(input = './5.12.3_sp_ds_spd_male_by_pauses_kmclust4.Rmd', output = './reports/5.12.3_sp_ds_spd_male_by_pauses_kmclust4.html'),
-  list(input = './5.12.4_sp_ds_spd_male_by_pauses_kmclust5.Rmd', output = './reports/5.12.4_sp_ds_spd_male_by_pauses_kmclust5.html'),
-
+  list(input = './5.12.4_sp_ds_spd_male_by_pauses_kmclust5.Rmd', output = './reports/5.12.4_sp_ds_spd_male_by_pauses_kmclust5.html')
+)
+  
+files_ds_kmdclust <- list(
   # kmdclust
   ## Все респонденты. Задание на точность.
   list(input = './4.13.1_ds_acc_by_all_kmdclust2.Rmd', output = './reports/4.13.1_ds_acc_by_all_kmdclust2.html'),
@@ -677,8 +619,10 @@ files <- list(
   list(input = './4.18.1_ds_spd_male_by_pauses_kmdclust2.Rmd', output = './reports/4.18.1_ds_spd_male_by_pauses_kmdclust2.html'),
   list(input = './4.18.2_ds_spd_male_by_pauses_kmdclust3.Rmd', output = './reports/4.18.2_ds_spd_male_by_pauses_kmdclust3.html'),
   list(input = './4.18.3_ds_spd_male_by_pauses_kmdclust4.Rmd', output = './reports/4.18.3_ds_spd_male_by_pauses_kmdclust4.html'),
-  list(input = './4.18.4_ds_spd_male_by_pauses_kmdclust5.Rmd', output = './reports/4.18.4_ds_spd_male_by_pauses_kmdclust5.html'),
+  list(input = './4.18.4_ds_spd_male_by_pauses_kmdclust5.Rmd', output = './reports/4.18.4_ds_spd_male_by_pauses_kmdclust5.html')
+)  
   
+files_sp_kmdclust <- list(
   # Психодиагностика и кластеры
   ## Все респонденты. Задание на точность
   list(input = './5.13.1_sp_ds_acc_by_all_kmdclust2.Rmd', output = './reports/5.13.1_sp_ds_acc_by_all_kmdclust2.html'),
@@ -790,15 +734,31 @@ files <- list(
   list(input = './5.18.4_sp_ds_spd_male_by_pauses_kmdclust5.Rmd', output = './reports/5.18.4_sp_ds_spd_male_by_pauses_kmdclust5.html')
 )
 
-mclapply(files, render_file, mc.cores = detectCores())
+# system.time({mclapply(files_ds, render_file, mc.cores = detectCores())})
+# mclapply(files_ca, render_file, mc.cores = detectCores())
+# mclapply(files_sp_hclust, render_file, mc.cores = detectCores())
+# mclapply(files_sp_kmclust, render_file, mc.cores = detectCores())
+# mclapply(files_sp_kmdclust, render_file, mc.cores = detectCores())
 
 
-rmarkdown::render('./4.1.1_ds_acc_by_all_hclust2.Rmd', output_file = './reports/4.1.1_ds_acc_by_all_hclust2.html')
-rmarkdown::render('./5.1.1_sp_ds_acc_by_all_hclust2.Rmd', output_file = './reports/5.1.1_sp_ds_acc_by_all_hclust2.html')
-rmarkdown::render('./5.1.1_sp_ds_acc_by_notcorr_hclust2.Rmd', output_file = './reports/5.1.1_sp_ds_acc_by_notcorr_hclust2.html')
-rmarkdown::render('./5.9.4_sp_ds_acc_male_by_all_kmclust5.Rmd', output_file = './reports/5.9.4_sp_ds_acc_male_by_all_kmclust5.html')
-rmarkdown::render('./5.13.1_sp_ds_acc_by_all_kmdclust2.Rmd', output_file = './reports/5.13.1_sp_ds_acc_by_all_kmdclust2.html')
-rmarkdown::render('./5.13.1_sp_ds_acc_by_notcorr_kmdclust2.Rmd', output_file = './reports/5.13.1_sp_ds_acc_by_notcorr_kmdclust2.html')
+sd <- read.csv("./reports/sign_differences.csv")
+
+files_sp <- gsub(pattern = "^5", replacement = "4", x = unique(sd$filename))
+files_sp <- gsub(pattern = "_sp_ds_", replacement = "_ds_", x = files_sp)
+
+files_ds <- lapply(files_sp, function(name) {
+  list(
+    input = paste0('./', name, '.Rmd'),
+    output = paste0('./reports/', name, '.html')
+  )
+})
+
+mclapply(files_ds, render_file, mc.cores = detectCores() - 2)
+# mclapply(files_ds_hclust, render_file, mc.cores = detectCores())
+# mclapply(files_ds_kmclust, render_file, mc.cores = detectCores())
+# mclapply(files_ds_kmdclust, render_file, mc.cores = detectCores())
 
 
 
+# rmarkdown::render('./4.1.1_ds_acc_by_all_hclust2.Rmd', output_file = './reports/4.1.1_ds_acc_by_all_hclust2.html')
+# rmarkdown::render('./5.1.1_sp_ds_acc_by_all_hclust2.Rmd', output_file = './reports/5.1.1_sp_ds_acc_by_all_hclust2.html')
